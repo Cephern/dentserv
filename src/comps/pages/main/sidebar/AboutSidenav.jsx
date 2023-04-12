86ac1251e24ca9bb4dfe2563c1ece407db796642
JagMax <gewoonmaks@gmail.com>
2023-04-12 16:21:49 +0300

    Phonk

---
+++ src/comps/pages/main/sidebar/AboutSidenav.jsx
@@ -0,0 +1,23 @@
+import styles from "../../../../styles/SecondaryNav.module.css";
+
+function AboutSidenav({ isActive }) {
+  return (
+    <ul className={`${styles.sidenav} ${isActive ? styles.active : ""}`}>
+      <h3>О Центре</h3>
+      <li>Принципы работы</li>
+      <li>Пресса о нас</li>
+      <li>№1 в рейтинге КоммерсантЪ</li>
+      <li>Зуботехническая лаборатория</li>
+      <li>Безопасность лечения зубов</li>
+      <li>Преимущества нашего Центра</li>
+      <li>Неудобства нашего Центра</li>
+      <li>Правила обслуживания</li>
+      <li>Направления деятельности</li>
+      <li>Независимые консультации</li>
+      <li>Взаимодействие с другими клиниками</li>
+      <li>Вакансии</li>
+    </ul>
+  );
+}
+
+export default AboutSidenav;
---
+++ src/comps/pages/main/sidebar/ServicesSidenav.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import styles from "../../../../styles/SecondaryNav.module.css";
+import ImplantSidenav from "./ImplantSidenav";
+
+const defaultSidenavStatus = {
+  implant: true,
+};
+
+function ServicesSidenav({ isActive }) {
+  const [sidenavActiveStatus, setSidenavActiveStatus] =
+    useState(defaultSidenavStatus);
+
+  const toggleSidenavStatus = (e) => {
+    const sidenavToToggle = e.target.getAttribute("data-name");
+    setSidenavActiveStatus((prev) => ({
+      ...defaultSidenavStatus,
+      [sidenavToToggle]: !prev[sidenavToToggle],
+    }));
+  };
+
+  return (
+    <ul className={`${styles.sidenav} ${isActive ? styles.active : ""}`}>
+      <h3>Услуги</h3>
+      <li
+        className={sidenavActiveStatus.implant ? styles.secondaryActive : ""}
+        data-name={"implant"}
+        onClick={(e) => toggleSidenavStatus(e)}
+      >
+        Имплантация
+        <ImplantSidenav isActive={sidenavActiveStatus.implant && isActive} />
+      </li>
+      <li>Протезы на Имплантах</li>
+      <li>Протезирование зубов</li>
+      <li>Хирургия</li>
+      <li>ЛОР-стоматология</li>
+      <li>Лечение зубов</li>
+      <li>Пародонтология</li>
+      <li>Ортодонтия</li>
+      <li>Гнатология</li>
+      <li>Косметология</li>
+      <li>Детское отделение</li>
+    </ul>
+  );
+}
+
+export default ServicesSidenav;
