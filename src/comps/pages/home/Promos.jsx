cadf8dfa930aac04c0109fd9e58290a0f254a260
JagMax <gewoonmaks@gmail.com>
2023-04-23 23:46:39 +0300

    new slider

--- src/comps/pages/home/Promos.jsx
+++ src/comps/pages/home/Promos.jsx
@@ -1,6 +1,6 @@
 import styles from "../../../styles/homePage/Promos.module.css";
 
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 
 import { data } from "../../../context/promosData.jsx";
 
@@ -24,27 +24,47 @@ const setPosition = (positionArr) => {
 function Promos() {
   const [promos, setPromos] = useState(data);
   const [currentPromo, setCurrentPromo] = useState(0);
-  const [isFading, setIsFading] = useState(false);
   const [autoScroll, setAutoScroll] = useState(true);
+  const [offset, setOffset] = useState(0);
+
+  const contentRef = useRef(null);
+
+  useEffect(() => {
+    window.addEventListener("load", () => {
+      setOffset(
+        contentRef.current.children[0].getBoundingClientRect().width + 80
+      );
+    });
+    window.addEventListener("resize", () => {
+      setOffset(contentRef.current.children[0].clientWidth + 80);
+    });
+
+    return () => {
+      window.removeEventListener("resize", () => {
+        setOffset(contentRef.current.children[0].clientWidth + 80);
+      });
+
+      window.removeEventListener("load", () => {
+        setOffset(
+          contentRef.current.children[0].getBoundingClientRect().width + 80
+        );
+      });
+    };
+  }, []);
 
   const handleLeftArrowClick = () => {
     setAutoScroll(false);
-    setIsFading(true);
     setTimeout(() => {
       if (currentPromo > 0) setCurrentPromo((prev) => prev - 1);
       if (currentPromo === 0) setCurrentPromo(promos.length - 1);
-
-      setIsFading(false);
     }, 300);
   };
 
   const handleRightArrowClick = () => {
     setAutoScroll(false);
-    setIsFading(true);
     setTimeout(() => {
       if (currentPromo < promos.length - 1) setCurrentPromo((prev) => prev + 1);
       if (currentPromo === promos.length - 1) setCurrentPromo(0);
-      setIsFading(false);
     }, 300);
   };
 
@@ -52,10 +72,8 @@ function Promos() {
     setAutoScroll(false);
     if (id === currentPromo) return;
 
-    setIsFading(true);
     setTimeout(() => {
       setCurrentPromo(id);
-      setIsFading(false);
     }, 300);
   };
 
@@ -63,7 +81,6 @@ function Promos() {
     let interval;
     if (autoScroll) {
       interval = setInterval(() => {
-        setIsFading(true);
         setTimeout(() => {
           setCurrentPromo((prev) => {
             if (prev === promos.length - 1) {
@@ -72,7 +89,6 @@ function Promos() {
               return prev + 1;
             }
           });
-          setIsFading(false);
         }, 300);
       }, 3000);
     }
@@ -83,76 +99,71 @@ function Promos() {
   return (
     <div className={styles.Promos}>
       <div className={styles.slider}>
-        <div className={styles.content}>
-          <svg
-            className={styles.left_arrow}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            onClick={handleLeftArrowClick}
-          >
-            <path d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM271 135c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-87 87 87 87c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L167 273c-9.4-9.4-9.4-24.6 0-33.9L271 135z" />
-          </svg>
-
-          <svg
-            className={styles.right_arrow}
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 512 512"
-            onClick={handleRightArrowClick}
-          >
-            <path d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM241 377c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l87-87-87-87c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L345 239c9.4 9.4 9.4 24.6 0 33.9L241 377z" />
-          </svg>
-
-          <div
-            className={`${styles.promo} ${isFading ? styles.fading : ""}`}
-            onClick={() => {
-              setAutoScroll(false);
-            }}
-          >
-            <img src={promos[currentPromo].promoPhoto} alt="Promo1" />
-
-            {promos.map((promo) => (
-              <img
-                key={promo.id}
-                style={{ display: "none" }}
-                src={promo.promoPhoto}
-                alt="preload"
-              ></img>
-            ))}
-
-            <div
-              className={styles.promo_text}
-              style={{
-                ...setPosition(promos[currentPromo].promoText.position),
-              }}
-            >
-              <h3>Акция</h3>
-              <h4>{promos[currentPromo].promoText.h4}</h4>
-              <ul>
-                {promos[currentPromo].promoText.ul.map((text) => (
-                  <li key={text}>{text}</li>
-                ))}
-              </ul>
-
-              <div className={styles.btns}>
-                <button>Подробнее</button>
-                <button>Записаться на приём</button>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className={styles.counter}>
+        <div
+          ref={contentRef}
+          className={styles.content}
+          style={{ left: `-${offset * currentPromo}px` }}
+        >
           {promos.map((promo) => (
             <div
               key={promo.id}
-              className={`${styles.counterItem} ${
-                promo.id === currentPromo ? styles.active : ""
-              }`}
-              style={{ width: `${100 / promos.length}%` }}
-              onClick={() => handleManualChange(promo.id)}
-            ></div>
+              className={styles.promo}
+              onClick={() => setAutoScroll(false)}
+            >
+              <img src={promo.promoPhoto} alt={`Promo Photo ${promo.id}`} />
+
+              <div
+                className={styles.promoText}
+                style={{
+                  ...setPosition(promo.promoText.position),
+                }}
+              >
+                <h3>Акция</h3>
+                <h4>{promo.promoText.h4}</h4>
+                <ul>
+                  {promo.promoText.ul.map((text) => (
+                    <li key={text}>{text}</li>
+                  ))}
+                </ul>
+
+                <div className={styles.btns}>
+                  <button>Подробнее</button>
+                  <button>Записаться на приём</button>
+                </div>
+              </div>
+            </div>
           ))}
         </div>
+      </div>
+      <div className={styles.counter}>
+        {promos.map((promo) => (
+          <div
+            key={promo.id}
+            className={`${styles.counterItem} ${
+              promo.id === currentPromo ? styles.active : ""
+            }`}
+            style={{ width: `${100 / promos.length}%` }}
+            onClick={() => handleManualChange(promo.id)}
+          ></div>
+        ))}
+
+        <svg
+          className={styles.leftArrow}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+          onClick={handleLeftArrowClick}
+        >
+          <path d="M512 256A256 256 0 1 0 0 256a256 256 0 1 0 512 0zM271 135c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-87 87 87 87c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0L167 273c-9.4-9.4-9.4-24.6 0-33.9L271 135z" />
+        </svg>
+
+        <svg
+          className={styles.rightArrow}
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 512 512"
+          onClick={handleRightArrowClick}
+        >
+          <path d="M0 256a256 256 0 1 0 512 0A256 256 0 1 0 0 256zM241 377c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l87-87-87-87c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0L345 239c9.4 9.4 9.4 24.6 0 33.9L241 377z" />
+        </svg>
       </div>
     </div>
   );
